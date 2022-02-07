f1ba345f3f5e593124986cfcb0588ae1b982f3f7
Sifat Sultan <sifatsultan@gmail.com>
2022-02-07 04:07:40 +0000

    created first version of promise

--- promise/promise.js
+++ promise/promise.js
@@ -1,6 +1,21 @@
-function DisplayName(name)
-{
-    console.log("Hello "+name);
-}
+let htmlContent = `<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
 
-DisplayName("Sifat")
+<h1>This is a Heading</h1>
+<p>This is a paragraph.</p>
+
+</body>
+</html>`;
+
+let fileResultPromise = new Promise(function(myResolve, myReject){
+  setTimeout(function(){myResolve(htmlContent)}, 3000);
+});
+
+fileResultPromise.then(function(value){
+  console.log("Fetched file succesfully");
+  console.log(value)
+})
