8e96deb4a5c260c31e547d885499a3e2c477fdee
Sifat Sultan <sifatsultan@gmail.com>
2022-03-29 09:47:13 +0600

    sorting added

--- react/my-app/src/index.js
+++ react/my-app/src/index.js
@@ -22,14 +22,14 @@ class Board extends React.Component {
 
     render() {
         var rows = []
-        let squareCounter = 0 ;
-        for (let j = 0; j < 3 ; j++) {
+        let squareCounter = 0;
+        for (let j = 0; j < 3; j++) {
             let row = [] // empty array of columns
             for (let i = 0; i < 3; i++) {
                 row.push(this.renderSquare(squareCounter))
-                squareCounter ++;
+                squareCounter++;
             }
-            rows.push(<div className='board-row'>{row}</div>)
+            rows.push(<div key={j} className='board-row'>{row}</div>)
         }
         return (
             <div>{rows}</div>
@@ -51,6 +51,19 @@ class Game extends React.Component {
         };
 
     }
+
+    sortTable(table_id, sortColumn) {
+        var tableData = document.getElementById(table_id).getElementsByTagName('tbody').item(0);
+        var rowData = tableData.getElementsByTagName('tr');
+        for (var i = 0; i < rowData.length - 1; i++) {
+            for (var j = 0; j < rowData.length - (i + 1); j++) {
+                if (Number(rowData.item(j).getElementsByTagName('td').item(sortColumn).innerHTML.replace(/[^0-9\.]+/g, "")) < Number(rowData.item(j + 1).getElementsByTagName('td').item(sortColumn).innerHTML.replace(/[^0-9\.]+/g, ""))) {
+                    tableData.insertBefore(rowData.item(j + 1), rowData.item(j));
+                }
+            }
+        }
+    }
+
 
     handleClick(i) {
         const history = this.state.history.slice(0, this.state.stepNumber + 1);
@@ -83,7 +96,6 @@ class Game extends React.Component {
         const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const winner = calculateWinner(current.squares);
-
 
         const moves = history.map((step, move) => {
             const desc = move ?
@@ -121,10 +133,10 @@ class Game extends React.Component {
                 </div>
                 <div className="game-info">
                     <div>{status}</div>
-                    <table>
+                    <table id="table-move">
                         <thead>
                             <tr>
-                                <th>Step</th>
+                                <th onClick={() => { this.sortTable("table-move", 0) }}>Step</th>
                                 <th>Jump</th>
                             </tr>
                         </thead>
